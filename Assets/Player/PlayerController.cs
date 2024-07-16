862b515e917a0fe4be64683a922e9f3fd3974f91
DreamDoggo <doggosanddragons@gmail.com>
2024-07-16 16:09:31 -0700

    JUMP OMG IT JUMPED SORT OF

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -11,51 +11,96 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float MoveForce = 15.0f;
     [SerializeField] float MaxVelocity = 6.0f;
     [SerializeField] float DampingCoefficient = 0.97f;
+    [SerializeField] float GroundedGraceDistance;
+    [SerializeField] float JumpVelocity = 6.0f;
+    [SerializeField] Transform GroundCheck;
 
-    Rigidbody2D rigidBody;
+    Rigidbody2D RefRigidBody;
+    BoxCollider2D RefCollider;
+
+    bool CanJump;
 
 
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        if (rigidBody == null ) 
+        RefRigidBody = GetComponent<Rigidbody2D>();
+        if (RefRigidBody == null)
         {
-            Debug.LogError("A rigidbody component was not found on the player");    
+            Debug.LogError("A rigidbody component was not found on the player");
+        }
+
+        RefCollider = GetComponent<BoxCollider2D>();
+        if (RefCollider == null) 
+        {
+            Debug.LogError("A BoxColider component was not found on the player");
         }
     }
 
     private void FixedUpdate()
+    {
+        UpdateMovement();
+        UpdateJump();
+    }
+
+    private void UpdateMovement() 
     {
         // use a variable to store where the player has to move
         // starts as (x=0, y=0) meaning our player has no velocity 
         Vector2 movementValues = Vector2.zero;
 
         // convert the user's input to movement values
-        if (Input.GetKey( MoveLeftKey ) ) 
+        if (Input.GetKey(MoveLeftKey))
         {
             movementValues += Vector2.left;
         }
-        else if (Input.GetKey( MoveRightKey ) ) 
+        else if (Input.GetKey(MoveRightKey))
         {
             movementValues += Vector2.right;
         }
 
         movementValues.Normalize();
-        rigidBody.AddForce( movementValues * MoveForce);
-
-        rigidBody.AddForce(movementValues * MoveForce);
+        RefRigidBody.AddForce(movementValues * MoveForce);
         // Clamp the maximum velocity to our defined cap.
-        if (rigidBody.velocity.magnitude > MaxVelocity)
+        if (RefRigidBody.velocity.magnitude > MaxVelocity)
         {
             // Maintain the current direction by using the normalized velocity vector
-            rigidBody.velocity = rigidBody.velocity.normalized * MaxVelocity;
+            RefRigidBody.velocity = RefRigidBody.velocity.normalized * MaxVelocity;
         }
         // We know the player let go of the controls if the input vector is nearly zero.
         if (movementValues.sqrMagnitude <= 0.1f)
         {
             // Quickly damp the movement when we let go of the inputs by multiplying the vector
             // by a value less than one each frame.
-            rigidBody.velocity *= DampingCoefficient;
+            RefRigidBody.velocity *= DampingCoefficient;
+        }
+    }
+
+    private void UpdateJump() 
+    {
+        Vector2 rayOrigin = GroundCheck.position;
+        Vector2 rayDirection = Vector2.down;
+        float rayDistance = .0001f;
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+        Debug.DrawRay(rayOrigin, rayDirection, Color.magenta);
+        if (rayHit) 
+        {
+            if (CanJump == false) { Debug.Log("Jump refreshed"); }
+            CanJump = true;
+            
+            if (RefRigidBody.velocity.y <= 0) 
+            {
+                
+            }
+        }
+
+        if (Input.GetKeyDown(JumpKey) && CanJump) 
+        {
+            Vector2 velocity = RefRigidBody.velocity;
+            velocity.y = JumpVelocity;
+            RefRigidBody.velocity = velocity;
+            CanJump = false;
+            Debug.Log("Jump used");
         }
 
     }
